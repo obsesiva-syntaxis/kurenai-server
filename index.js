654b92750f63103a9e99566a761a592a3762cf12
obsesiva-syntaxis <obsesivasyntaxis@gmail.com>
2022-02-21 21:06:21 -0300

    production!

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ async function server(){
         config.application.cors.server
     ));
 
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+        next();
+    });
+
     app.use(graphqlUploadExpress());
     serverApollo.applyMiddleware({ app });
     await new Promise((r) => app.listen({
